c3e69fe3519b3f568339e4ed41cd775e43a612a8
SergioGarces7 <117357799+SergioGarces7@users.noreply.github.com>
2024-03-07 15:01:19 +0100

    Componente MisSalas creado

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraYMenuComponent } from './cabecera-y-menu/cabecera-y-menu.component';
+import { MisSalasComponent } from './mis-salas/mis-salas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraYMenuComponent],
+  imports: [RouterOutlet, CabeceraYMenuComponent, MisSalasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ front/src/app/mis-salas/mis-salas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-salas',
+  standalone: true,
+  imports: [],
+  templateUrl: './mis-salas.component.html',
+  styleUrl: './mis-salas.component.css'
+})
+export class MisSalasComponent {
+
+}
