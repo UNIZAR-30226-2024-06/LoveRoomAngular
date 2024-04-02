e779fdfa4db1b55dd3b135d0a5278a68e474a3db
Julve <840710@unizar.es>
2024-04-02 13:53:07 +0200

    Pequeños cambios hechos para intentar unir el login con el back end, aún sin términar

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, HttpClientModule],
+  providers: [HttpClient],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
 
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    this.http.post<any>('http://localhost:3000/user/login', { username, password })
+      .subscribe(response => {
+        // Maneja la respuesta del backend
+        if (response && response.token) {
+          localStorage.setItem('token', response.token); // Almacena el token en el almacenamiento local
+          // Redirige al usuario a la página principal o a donde sea necesario
+        }
+      });
+  }
 }
