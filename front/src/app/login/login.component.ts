96e3d8be678b44fd5338565d66caa65cec9e1134
SergioGarces7 <117357799+SergioGarces7@users.noreply.github.com>
2024-04-03 19:34:37 +0200

    manda bien a la base de datos, no sabemos mostrar el token o no lo cogemos bien

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { routes } from '../app.routes';
 
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HttpClientModule],
+  imports: [RouterOutlet, RouterModule],
   providers: [HttpClient],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -14,16 +16,27 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 export class LoginComponent {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-  login(username: string, password: string) {
-    this.http.post<any>('http://localhost:3000/user/login', { username, password })
-      .subscribe(response => {
-        // Maneja la respuesta del backend
-        if (response && response.token) {
-          localStorage.setItem('token', response.token); // Almacena el token en el almacenamiento local
-          // Redirige al usuario a la página principal o a donde sea necesario
+  login(correo: string, contrasena: string) {
+    console.log('ENTRA')
+    this.http.post<any>('http://localhost:5000/user/login', { correo, contrasena })
+      .subscribe(
+        response => {
+          // Maneja la respuesta del backend
+          if (response && response.token) {
+            console.log('Inicio de sesión exitoso:', response.token);
+            localStorage.setItem('token', response.token); // Almacena el token en el almacenamiento local
+            // Redirige al usuario a la página principal o a donde sea necesario
+            
+          }
+          this.router.navigate(['/registrarse']);
+        },
+        error => {
+          this.router.navigate(['/registrarse']);
+          // Maneja el error, por ejemplo, mostrando un mensaje de error al usuario
+          console.error('Error en la solicitud de inicio de sesión:');
         }
-      });
+      );
   }
 }
