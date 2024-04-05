3276ab8cf1d5c52295651f2a2dd6630d91633387
Julve <840710@unizar.es>
2024-04-05 02:27:25 +0200

    Se ha creado conexión total con el backend a la hora de crear usuarios y se ha diseñado el sistema de cerrar sesión el cual cambia el icono de la cabecera y destruye el token que se tenía

--- front/src/app/cabecera-y-menu/cabecera-y-menu.component.ts
+++ front/src/app/cabecera-y-menu/cabecera-y-menu.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cabecera-y-menu',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './cabecera-y-menu.component.html',
   styleUrl: './cabecera-y-menu.component.css'
 })
 export class CabeceraYMenuComponent {
 
+  token: string | null = null;
+
+  constructor() {
+    // Verificar si estamos en el navegador antes de intentar acceder a localStorage
+    if (typeof window !== 'undefined') {
+      this.token = localStorage.getItem('token');
+    }
+  }
+
+  logout(): void {
+    // Verificar si estamos en el navegador antes de intentar acceder a localStorage
+    if (typeof window !== 'undefined') {
+      // Eliminar el token del localStorage
+      localStorage.removeItem('token');
+      window.location.reload();
+    }
+  }
 }
--- front/src/app/registrarse/registrarse.component.ts
+++ front/src/app/registrarse/registrarse.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-registrarse',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, FormsModule],
+  providers: [HttpClient],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.css'
 })
 export class RegistrarseComponent {
+  correo: string = '';
+  nombre: string = '';
+  contrasena: string = '';
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  registrarse(): void {
+    const credentials = {
+      correo: this.correo,
+      nombre: this.nombre,
+      contrasena: this.contrasena
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    this.http.post<any>('http://localhost:5000/user/create', credentials, { headers: headers })
+      .subscribe(
+        response => {
+          // Si la autenticación fue exitosa, muestra una alerta con el token.
+          console.log('Creación de cuenta exitosa', response);
+          if (response.token) {
+            // Guardar el token en localStorage
+            localStorage.setItem('token', response.token);
+            alert('¡Creación de cuenta exitosa! Token: ' + response.token);
+            this.router.navigate(['/']);
+          } else {
+            alert('Error: No se recibió token en la respuesta.');
+          }
+        },
+        error => {
+          // Si hubo un error durante la autenticación, muestra una alerta con el mensaje de error.
+          console.error('Error al crear cuenta', error);
+          alert('Error al crear cuenta: ' + error.message);
+          this.router.navigate(['/mis-salas']);
+        }
+      );
+  }
 }
