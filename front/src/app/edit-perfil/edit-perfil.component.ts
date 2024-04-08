c08097c6407dafd0b01823c764f80f2d0b015ad3
SergioGarces7 <117357799+SergioGarces7@users.noreply.github.com>
2024-04-09 01:11:47 +0200

    edit-perfil diseñada bien

--- front/src/app/edit-perfil/edit-perfil.component.ts
+++ front/src/app/edit-perfil/edit-perfil.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-perfil',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './edit-perfil.component.html',
   styleUrl: './edit-perfil.component.css'
 })
 export class EditPerfilComponent {
   edades: number[] = Array.from({length: 82}, (_, i) => i + 18);
   mostrarContrasena = false;
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  imagenPerfil = 'assets/Logo.png'; //Quiza aqui traer la de la BD
 
   toggleMostrarContrasena() {
     this.mostrarContrasena = !this.mostrarContrasena;
   }
+
+  
+
+  cambiarImagen(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagenPerfil = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  
 }
