0d903628239695b4064642e5b0ec515d34337a4e
Julve <840710@unizar.es>
2024-04-19 01:37:45 +0200

    Pequeñas modificaciones en mis salas

--- front/src/app/mis-salas/mis-salas.component.ts
+++ front/src/app/mis-salas/mis-salas.component.ts
@@ -3,6 +3,8 @@ import { CabeceraYMenuComponent } from '../cabecera-y-menu/cabecera-y-menu.compo
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mis-salas',
@@ -16,7 +18,7 @@ import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 export class MisSalasComponent implements OnInit {
   salas: any[] = []; // Aquí almacenaremos las salas obtenidas del backend
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+  constructor(private http: HttpClient, private router: Router, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     // Llamada al backend para obtener la lista de salas
@@ -42,10 +44,37 @@ export class MisSalasComponent implements OnInit {
         }
       );
   }
+
+  getVideoTitle(videoId: string) {
+    const apiKey = environment.apiKey;
+    const url = `https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&part=snippet&id=${videoId}`;
+    return this.http.get(url);
+  }
   
   getVideoThumbnailUrl(videoId: string): SafeResourceUrl {
     // Creamos la URL de la miniatura del video de YouTube y la pasamos a través de DomSanitizer
-    const url = `https://img.youtube.com/vi/${videoId}/default.jpg`;
+    const url = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
     return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  watchVideo(videoId: string) {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    // Hacer la solicitud HTTP GET al backend
+    this.http.get(`http://localhost:5000/videos/watch/${videoId}`, { headers }).subscribe(
+      (response: any) => {
+        // Manejar la respuesta del backend aquí
+        console.log(response);
+        // Navegar a la sala después de la verificación del backend
+        this.router.navigate(['/sala', videoId]);
+      },
+      (error: any) => {
+        // Manejar errores aquí
+        console.error(error);
+      }
+    );
   }
 }
