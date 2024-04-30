b832b544c992497752192f804382401e2fd1a7ec
Julve <840710@unizar.es>
2024-04-30 22:36:08 +0200

    Añadida casi por completo la funcionalidad de cambio de imagen de perfil

--- front/src/app/edit-perfil/edit-perfil.component.ts
+++ front/src/app/edit-perfil/edit-perfil.component.ts
@@ -17,28 +17,36 @@ export class EditPerfilComponent {
   mostrarContrasena = false;
   @ViewChild('fileInput') fileInput!: ElementRef;
   imagenPerfil = 'assets/Logo.png'; //Quiza aqui traer la de la BD
+  usuario: any;
+  error: string='';
+  flag_imagen = false;
+  file: any;
 
   toggleMostrarContrasena() {
     this.mostrarContrasena = !this.mostrarContrasena;
   }
 
-  
+  actualizarEdadMaxima() {
+    if (this.usuario.buscaedadmin > this.usuario.buscaedadmax) this.usuario.buscaedadmax = this.usuario.buscaedadmin;
+  }
+
+  actualizarEdadMinima() {
+    if (this.usuario.buscaedadmin > this.usuario.buscaedadmax) this.usuario.buscaedadmin = this.usuario.buscaedadmax;
+  }
+
 
   cambiarImagen(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files[0]) {
-      const file = input.files[0];
+      this.file = input.files[0];
       const reader = new FileReader();
       reader.onload = () => {
         this.imagenPerfil = reader.result as string;
       };
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(this.file);
+      this.flag_imagen = true;
     }
   }
-  
-  
-  usuario: any;
-  error: string='';
 
   constructor(private http: HttpClient, private router: Router) {
     this.obtenerPerfilUsuario();
@@ -52,7 +60,9 @@ export class EditPerfilComponent {
     this.http.get<any>('http://'+environment.host_back+'/user/profile', { headers: headers })
       .subscribe(
         response => {
+          console.log(response);
           this.usuario = response;
+          this.imagenPerfil = this.usuario.fotoperfil === 'null.jpg' ? this.imagenPerfil : this.usuario.fotoperfil;
         },
         error => {
           console.error('Error al obtener el perfil del usuario', error);
@@ -88,8 +98,27 @@ export class EditPerfilComponent {
       'Authorization': 'Bearer ' + localStorage.getItem('token')
     });
 
+    const token = localStorage.getItem('token');
+
     const body = this.usuarioActualizado; // Enviar el objeto usuarioActualizado al backend
     console.log(body);
+
+    console.log(this.file);
+    if (this.flag_imagen == true) {
+      this.http.post<any>('http://'+environment.host_back+'/multimedia/upload/foto/${token}', this.file, { headers: headers })
+      .subscribe(
+        response => {
+          console.log('Imagen subida', response);
+          alert("Imagen subida");
+          this.usuarioActualizado.fotoperfil = response;
+        },
+        error => {
+          console.error('Error al subir la imagen', error.message);
+          this.error = error.error.error;
+          alert(error.message);
+        }
+      );
+    }    
 
     this.http.put<any>('http://'+environment.host_back+'/user/update', body, { headers: headers })
       .subscribe(
