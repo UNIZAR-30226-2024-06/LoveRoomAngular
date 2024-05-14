e2b88985f3c3ccaee72a107e4212f0d2f8fba9fb
AlexBenedi <843826@unizar.es>
2024-05-15 01:50:11 +0200

    Solucion foto

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiKey: 'AIzaSyBOsDlrJWusYYGbZ7xXcyoRK2_KkGBVvfY',
-    //host_back: '48.216.156.246'
-    host_back: '127.0.0.1:5000'
+    host_back: '48.216.156.246'
+    //host_back: '127.0.0.1:5000'
 };
